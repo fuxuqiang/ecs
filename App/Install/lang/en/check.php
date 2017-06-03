a95595c19d6a5357cc9c5aedba36013d47c8335c
fuxuqiang <fuxuqiang@outlook.com>
2017-06-03 23:41:49 +0800

    Add files via upload

--- App/Install/lang/en/check.php
+++ App/Install/lang/en/check.php
@@ -8,10 +8,12 @@ return [
 	'config_system' => 'Config system',
 	'php_os' => 'Operating system',
 	'php_ver' => 'PHP version',
+	'does_support_mysqli' => 'Support MySQLi?',
 	'gd_version' => 'GD version',
 	'dir_priv_checking' => 'Check directory authorization',
 	'can_write' => 'Writable',
 	'cannt_write' => 'Not writable',
 	'not_exists' => 'Not exists',
+	'support' => 'Support',
 	'not_support' => 'No support',
 ];
