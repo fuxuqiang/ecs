8637a546cc40273b3fcbe5fa67cf411b54eb8db9
fuxuqiang <fuxuqiang@outlook.com>
2017-07-22 21:38:07 +0800

    增加运行时间统计

--- public/index.php
+++ public/index.php
@@ -4,17 +4,17 @@
 if(version_compare(PHP_VERSION, '5.4') < 0)
 	die('require PHP >= 5.4 !');
 
-// 定义当前根目录
+// 定义开始时间及项目根目录
+define('START', microtime(true));
 define('ROOT_PATH', strtr(dirname(__DIR__), '\\', '/').'/');
 
 // 自动载入类文件
-function __autoload($class) 
-{
+spl_autoload_register(function($class) {
 	if (strstr($class, '\\')) {
 		$class = strtr($class, '\\', '/');
 	}
 	require ROOT_PATH.$class.'.php';
-}
+});
 
 // 启动应用
 \Includes\App::start();
